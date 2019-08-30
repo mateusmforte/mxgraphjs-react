f719e590152c0081de83c61e07128b7bc14e64e7
romulofreires1 <romulofreires1@gmail.com>
2019-08-30 18:55:32 -0300

    Hello World

---
+++ src/mxgraph.js
@@ -0,0 +1,9 @@
+import mxgraph from 'mxgraph';
+
+const mg = mxgraph();
+
+export const mxClient = mg.mxClient;
+export const mxGraph = mg.mxGraph;
+export const mxEvent = mg.mxEvent;
+export const mxRubberband = mg.mxRubberband;
+export const mxUtils = mg.mxUtils;
